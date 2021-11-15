07e00923f994ceac0f6570bd9c1e565fc1d09c48
gilliganondata <tim.wilson@searchdiscovery.com>
2021-11-14 19:57:53 -0500

    Add thumbnails to the list page for image-randomization.

--- content/image-randomization/code/build_rmd.R
+++ content/image-randomization/code/build_rmd.R
@@ -4,7 +4,8 @@
 # Create an image ID that can be used as a unique identifier
 img_id <- gsub("(^.*images\\/)(.*)(_original.*$)", "\\2", img_out_path)
 
-# Create the YAML header
+# Create the YAML header. We're including the URLs for the original and
+# the transformed images so we can make images in the list page
 yaml <- paste("---",
               "title: Random Manipulation of a Random SmugMug Image",
               "author: Tim Wilson",
@@ -18,12 +19,14 @@ yaml <- paste("---",
               "  - Twitter",
               "  - ImageMagick",
               "type: image-randomization",
+              paste0("img-original: ", gsub(rel_path,"",img_out_path)),
+              paste0("img-transformed: ", gsub(rel_path,"",img_trans_out_path)),
               "---\n\n",
               sep = "\n")
 
 
 # Intro text
-intro <- "For details on the nuts and bolts behind this project, see <a href=\"post/2021/11/07/random-image-manipulation/\" target = \"_blank\"</a>\n"
+intro <- "For details on the nuts and bolts behind this project, see <a href=\"post/2021/11/07/random-image-manipulation/\" target = \"_blank\">this post</a>.\n"
 
 # Create a string for the original image. 
 orig_loc <- paste0("Original image source: ", img_url, "\n\n")
