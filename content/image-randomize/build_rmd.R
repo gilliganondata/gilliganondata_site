3047b1cf689e532a2e0d04c5580e2d4189a8cf31
gilliganondata <tim.wilson@searchdiscovery.com>
2021-11-29 11:54:13 -0500

    Clear all output to date. Change file structure to remove "imgrandomize" and "imgrandomize/R" for just "image-randomize". Set cron back to hourly for a bit.

---
+++ content/image-randomize/build_rmd.R
@@ -0,0 +1,56 @@
+# Script to actually build the .Rmd file that will be a new page for the specific
+# image
+
+# Create an image ID that can be used as a unique identifier
+img_id <- gsub("(^.*images\\/)(.*)(_original.*$)", "\\2", img_out_path)
+
+# Create the YAML header. We're including the URLs for the original and
+# the transformed images so we can make images in the list page
+yaml <- paste("---",
+              "title: Random Manipulation of a Random SmugMug Image",
+              "author: Tim Wilson",
+              paste0("date: ", as.character(Sys.Date())),
+              paste0("slug: random-image-", img_id),
+              "categories:",
+              "  - R",
+              "tags:",
+              "  - Github Actions",
+              "  - SmugMug",
+              "  - Twitter",
+              "  - ImageMagick",
+              "type: image-randomization",
+              paste0("img_original: ", gsub(rel_path,"",img_out_path)),
+              paste0("img_transformed: ", gsub(rel_path,"",img_trans_out_path)),
+              "---\n\n",
+              sep = "\n")
+
+
+# Intro text
+intro <- "For details on the nuts and bolts behind this project, see <a href=\"/post/2021/11/07/random-image-manipulation/\" target = \"_blank\">this post</a>.\n"
+
+# Create a string for the original image. 
+orig_loc <- paste0("Original image source: ", img_url, "\n\n")
+
+# Show the original image. 
+orig_img <- paste0("![](/image-randomize/",gsub(rel_path,"",img_out_path),"){width=100%}\n\n")
+
+# List the transformations performed
+trans_perf <- paste("Transformations performed:\n",
+                    trans_description,
+                    "\n",
+                    "The resulting image:",
+                    "\n",
+                    sep = "\n")
+
+# Show the final image
+final_img <- paste0("![](/image-randomize/",gsub(rel_path,"",img_trans_out_path),"){width=100%}\n")
+
+# Create the output file
+write(paste(yaml, 
+            intro,
+            orig_loc, 
+            orig_img,
+            trans_perf,
+            final_img),
+      file = paste0(rel_path, "output/", Sys.Date(), "-", img_id, ".Rmd"),
+      append = FALSE)
